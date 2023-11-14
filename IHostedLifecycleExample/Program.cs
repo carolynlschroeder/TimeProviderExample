49722f7f12cab62e321b8c706df14c5b532629a9
Carolyn Schroeder <carolynlschroeder@hotmail.com>
2023-11-14 10:59:38 -0600

    Completed IHostedLifecycleService

---
+++ IHostedLifecycleExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using IHostedLifecycleExample;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureAppConfiguration((hostContext, configApp) =>
+{
+    configApp.AddJsonFile("appsettings.json", true);
+})
+.ConfigureLogging((context, b) => b.AddConsole())
+.ConfigureServices((hostContext, services) =>
+ {
+     services.AddHostedService<ServiceA>();
+
+ });
+IHost host = builder.Build();
+host.Run();
+
---
+++ IHostedLifecycleExample/ServiceA.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace IHostedLifecycleExample
+{
+    public class ServiceA: IHostedService, IHostedLifecycleService
+    {
+        private readonly ILogger<ServiceA> logger;
+
+        public ServiceA(ILogger<ServiceA> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task StartingAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Starting Async");
+
+            return Task.CompletedTask;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Start Async");
+            _ = DoWork();
+            return Task.CompletedTask;
+        }
+
+        public Task StartedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StoppingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public Task StoppedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        public async Task DoWork(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            logger.LogInformation("Started DoWork");
+            await Task.Delay(5000);
+            Environment.Exit(0);
+        }
+    }
+}
